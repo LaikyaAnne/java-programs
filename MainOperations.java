4766b179fb864d805a2cacb6fb61c449620b55de
A. Laikya sai sri <24B11AI013@adityauniversity.in>
2026-03-24 09:54:02 +0530

    Add files via upload

---
+++ MainOperations.java
@@ -0,0 +1,10 @@
+import Mypack.Operations;
+class MainOperations
+{
+public static void main(String[] args){
+Operations op=new Operations();
+System.out.println("Addition: "+op.add(20,30));
+System.out.println("Subtraction: "+op.sub(10,2));
+System.out.println("Multiplication: "+op.mul(5,5));
+}
+}
